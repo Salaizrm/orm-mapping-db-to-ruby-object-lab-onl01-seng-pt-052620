9a66a64aedf977fe5c563b9e80e87473fc11379d
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-05 23:27:00 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -30,6 +30,10 @@ class Student
     WHERE name = ?
     LIMIT 1
     SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end
     # find the student in the database given a name
     # return a new instance of the Student class
   end
