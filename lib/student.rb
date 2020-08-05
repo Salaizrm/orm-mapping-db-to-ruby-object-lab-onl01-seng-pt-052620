33d10aa6efaaaad1431e08b45b1f63742897b428
Robert Salaiz <work.salaizrm@gmail.com>
2020-08-05 23:29:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -38,6 +38,16 @@ class Student
     # return a new instance of the Student class
   end
 
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
